f417e020a7379fef8992cec7658a5ee442aaa9e6
David Stewart <davidstewart@gmail.com>
2019-02-12 02:49:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-
+require 'pry'
 
 def fizzbuzz(x)
   three = x / 3.0
   five = x / 5.0
   three
   five
+  binding.pry
 end
